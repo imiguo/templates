2cc1eadde791c3262e58ac91a846cfe7e8a9510b
entimm <entimm@gmail.com>
2017-08-24 18:14:14 +0800

    bitcoin confirm form

--- sinpayer/tmpl/deposit_bitcoin_confirm.blade.php
+++ sinpayer/tmpl/deposit_bitcoin_confirm.blade.php
@@ -1,15 +1,26 @@
 {%include file="header.blade.php"%}
 
 {%if $false_data != 1%}
-    <h3>Please confirm your deposit:</h3>
+    <h3>Please confirm your deposit</h3>
     <br>
     <br>
-    Amount ($US): <b>{%$amount_format%}</b><br>
-        
-    Send us to address: <i><a href="bitcoin:{%$settings.def_payee_account_bitcoin%}?message=Deposit+to+{%$settings.site_name%}+User+{%$userinfo.username%}">{%$settings.def_payee_account_bitcoin%}</a></i>
-    <br>
-    <br>
-    <img id="coin_payment_image" src="{%$settings.def_payee_qrcode_bitcoin%}">
+    <form action="https://www.asmoney.com/sci.aspx" method="post">
+
+        Your Bitcoin account: <b>{%$userinfo.bitcoin%}</b><br>
+        Amount ($US): <b>{%$amount_format%}</b><br>
+
+        <input type="hidden" name="USER_NAME" value="{%$settings.def_payee_username_bitcoin%}"/>
+        <input type="hidden" name="STORE_NAME" value="{%$settings.def_payee_storename_bitcoin%}"/>
+        <input type="hidden" name="PAYMENT_UNITS" value="USD"/>
+        <input type="hidden" name="PAYMENT_AMOUNT" value="{%$amount%}"/>
+        <input type="hidden" name="PAYMENT_ID" value="{%$payment_id%}"/>
+        <input type="hidden" name="PAYMENT_METHOD" value="BITCOIN"/>
+        <input type="hidden" name="PAYMENT_MEMO" value="Deposit to {%$settings.site_name%} User {%$userinfo.username%}"/>
+
+        <br>
+        <input type=submit name=i_submit value="Process" class=sbmt> &nbsp;
+        <input type=button class=sbmt value="Cancel" onclick="document.location='?a=account'">
+    </form>
 {%/if%}
 {%include file="footer.blade.php"%}
 
