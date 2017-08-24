0c2a3fad25a653b93df54944b98225e018167a09
entimm <entimm@gmail.com>
2017-08-24 18:13:59 +0800

    统一 payment status

--- sinpayer/tmpl/deposit_egold_status.blade.php
+++
@@ -1,5 +0,0 @@
-{%include file="header.blade.php"%}
-<h3>Your Deposit status</h3><br>
-<br>
-{%if $process eq 'yes'%} We have received your deposit. Thank you! {%else%} We have 
-not received your deposit. Please try again. {%/if%} {%include file="footer.blade.php"%}
--- sinpayer/tmpl/deposit_status.blade.php
+++ sinpayer/tmpl/deposit_status.blade.php
@@ -1,12 +1,9 @@
 {%include file="header.blade.php"%}
-
 <h3>Your Deposit status</h3><br>
 <br>
 {%if $process eq 'yes'%}
-  We have received your deposit. Thank you! 
+  We have received your deposit. Thank you!
 {%else%}
-  We have 
-  not received your deposit. Please try again. 
-{%/if%} 
-
+  We have not received your deposit. Please try again.
+{%/if%}
 {%include file="footer.blade.php"%}
